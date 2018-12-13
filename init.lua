1a6da4415dced688042e932f43e1adfd9bc7da7e
Ferrum Princeps I <ferrumprinceps@politician.com>
2018-12-12 18:42:26 -0600

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+-----------------------------------WARSHIPS MOD FOR MINETEST--------------------------
+local default_path = minetest.get_modpath("warships")
+
+dofile(default_path.."/trireme.lua")
+dofile(default_path.."/longboat.lua")
+dofile(default_path.."/galley.lua")
+dofile(default_path.."/canoe.lua")
+dofile(default_path.."/.lua")
+dofile(default_path.."/.lua")
+dofile(default_path.."/.lua")
+dofile(default_path.."/.lua")
+dofile(default_path.."/.lua")
